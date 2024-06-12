91a70b364c3192cf7e4d5c4ad8373801024c676b
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-06-12 11:26:00 +0700

    done tailwindcss & antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { DatePicker } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload helo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-[red] text-[50px] font-bold">
+      <p>HELLO WORLD</p>
+      <DatePicker />
     </div>
   );
 }
